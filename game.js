34a0c701800601d83ed36aed000831cdde64f1df
janzeteachesit <janzeteachesit@gmail.com>
2017-05-08 13:21:46 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-aquaplane.js
+var game = new Phaser.Game(1200, 900, Phaser.AUTO, 'aquaplane');
+
+game.state.add('Aquaplane.Preloader', Aquaplane.Preloader);
+game.state.add('Aquaplane.MainMenu', Aquaplane.MainMenu);
+game.state.add('Aquaplane.Game', Aquaplane.Game);
+
+game.state.start('Aquaplane.Preloader');
