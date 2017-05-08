e1963d19c086af10c39d8d09a8e34ed1cf21cc78
janzeteachesit <janzeteachesit@gmail.com>
2017-05-08 14:01:20 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,2 @@
-var game = new Phaser.Game(640, 480, Phaser.AUTO, '', {
-  preload: preload,
-  create: create,
-  update: update
-});
+var game = new Phaser.Game(640, 480, Phaser.AUTO, 'Aquaplane');
+game.state.add('aquaplane', aquaplane.js);
